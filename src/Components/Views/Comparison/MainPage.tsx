383337a0b758f50e940b8cdd5136e7dffc389db5
Abdullah-167 <abdullahpadhana503@gmail.com>
2023-09-23 17:25:42 +0500

    commit done

--- src/Components/Views/Comparison/MainPage.tsx
+++ src/Components/Views/Comparison/MainPage.tsx
@@ -1,20 +1,44 @@
 import Container from '@/Components/Layout/Container'
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { AiFillFacebook } from 'react-icons/ai'
 import { FaTwitterSquare } from 'react-icons/fa'
 import { FiLink2 } from 'react-icons/fi'
 
 const MainPage = () => {
+
+    const [showItems, setShowItems] = useState(true);
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth < 1024) {
+                setShowItems(false);
+            } else {
+                setShowItems(true);
+            }
+        };
+
+        // Add event listener to update showItems on window resize
+        window.addEventListener('resize', handleResize);
+
+        // Initial check on component mount
+        handleResize();
+
+        // Clean up the event listener when the component unmounts
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <section>
-            <div className='flex gap-3 justify-between items-center'>
-                <span className='text-[#019943] cursor-pointer flex gap-4'>
+            <div className='flex md:flex-nowrap flex-wrap justify-between items-center'>
+                <span className='text-[#019943] cursor-pointer hidden md:flex sm:gap-4 sm:text-base text-xs'>
                     {`Mobile Prices >`}
                     <span>{`Compare Mobile Phones > `}</span>
                     <span>{` Samsung Galaxy Z Fold >      `}</span>
                 </span>
-                <div className='flex gap-2 text-3xl items-center'>
+                <div className='flex gap-2 text-3xl items-center sm:mb-0 mb-5'>
                     <span className='text-sm cursor-pointer'>Share On</span>
                     <span className='cursor-pointer'>  <FiLink2 /> </span>
                     <span className='text-blue-500 cursor-pointer'>
@@ -25,8 +49,8 @@ const MainPage = () => {
                     </span>
                 </div>
             </div>
-            <div className='flex justify-between border'>
-                <div className='pt-3 px-3 max-w-[400px] border-r pr-[77px]'>
+            <div className='flex lg:justify-between justify-end flex-grow border first-letter  sm:mb-0 mb-7'>
+                <div className='pt-3 px-3 max-w-[400px] border-r pr-[64px] lg:block hidden'>
                     <p className='text-gray-600 text-lg font-semibold pb-3'>Compare By:</p>
                     {specs.map((item, index) => {
                         return (
@@ -38,16 +62,18 @@ const MainPage = () => {
                         )
                     })}
                 </div>
-                <div className='flex'>
+                <div className='flex justify-end flex-grow'>
                     {mobleSpecs.map((item, index) => {
                         return (
-                            <div className='px-1 py-1 border-r border-r-gray-300'
-                                key={index}>
+                            <div
+                                className={`px-1 py-1 border-r border-r-gray-300 max-w-[291px] xl:max-w-[350px] 2xl:max-w-[400px] w-full ${!showItems && index >= 2 ? 'hidden' : ''}`}
+                                key={index}
+                            >
                                 <div className='bg-gray-100 px-3 py-1 mb-2'>
                                     <h2 className='text-[13px] font-semibold text-gray-600 pb-2'>{item.heading}</h2>
-                                    <div className='flex gap-1 pb-2'>
-                                        <input className='outline-none border border-gray-400 px-2 text-xs' type={item.input} />
-                                        <span className='bg-[#f9f9f9] text-xs px-2 py-1 border border-gray-400 cursor-pointer'>{item.btn}</span>
+                                    <div className='flex sm:flex-nowrap flex-wrap gap-1 pb-2'>
+                                        <input className='outline-none border border-gray-400 px-2 text-xs w-full' type={item.input} />
+                                        <span className='bg-[#f9f9f9] text-xs px-2 py-1 border border-gray-400 cursor-pointer w-full max-w-[65px]'>{item.btn}</span>
                                     </div>
                                     <div className='flex items-center gap-1 text-xs'>
                                         <span className='text-sm'>&#9432;</span>
@@ -55,16 +81,16 @@ const MainPage = () => {
                                     </div>
                                 </div>
                                 <div className='px-3'>
-                                    <p className='text-xl pb-2'>{item.modelName}</p>
-                                    <div className='flex items-center gap-4 justify-between pb-2'>
-                                        <Image className='w-[500px] 2xl:w-[500px]' src={item.modelImg} alt={''} width={111} height={100} />
+                                    <p className='sm:text-lg lg:text-xl pb-2'>{item.modelName}</p>
+                                    <div className='flex sm:flex-nowrap flex-wrap items-center gap-4 justify-center sm:justify-between pb-2'>
+                                        <Image className='' src={item.modelImg} alt={''} width={111} height={100} />
                                         <div className=' px-3 py-1'>
-                                            <p className='bg-gray-100 cursor-pointer px-1 pb-1 mb-[1px]'>{item.reviews}</p>
-                                            <p className='bg-gray-100 cursor-pointer px-1 pb-1 mb-[1px]'>{item.specs}</p>
-                                            <p className='bg-gray-100 cursor-pointer px-1 pb-1 mb-[1px]'>{item.pic}</p>
+                                            <p className='bg-gray-100 cursor-pointer px-1 pb-1 mb-[1px] lg:text-base text-sm'>{item.reviews}</p>
+                                            <p className='bg-gray-100 cursor-pointer px-1 pb-1 mb-[1px] lg:text-base text-sm'>{item.specs}</p>
+                                            <p className='bg-gray-100 cursor-pointer px-1 pb-1 mb-[1px] lg:text-base text-sm'>{item.pic}</p>
                                         </div>
                                     </div>
-                                    <div className='flex items-center gap-2'>
+                                    <div className='flex sm:flex-nowrap flex-wrap justify-center sm:justify-between items-center gap-2'>
                                         <span>{item.price}</span>
                                         {item.com && (
                                             <p className='text-xs'>(COMING SOON)</p>
@@ -78,71 +104,59 @@ const MainPage = () => {
             </div>
             <div className=' border border-[#019943] '>
                 {specs.map((item, index) => (
-                    <table className='w-full border-b border-b-[#019943]' key={index}>
-                        <tbody>
-                            {item.dataOne.map((newItem, idx) => (
-                                <tr key={idx}>
-                                    <th className='w-[150px] text-lg bg-[#a8d8a0] text-[#595959] font-semibold text-left pl-3 border-[#019943]'>{item.heading}</th>
-                                    <td className='w-[160px] bg-[#E5F9DB] px-2 py-1 text-base font-semibold border-b border-b-[#019943] text-[#676767]'>{newItem.heading}</td>
-                                    <td>
-                                        <table className='w-full'>
-                                            <tbody>
-                                                {newItem.innerData.map((innerItem, innerIdx) => (
-                                                    <tr key={innerIdx}>
-                                                        <td className='py-1 pl-5 border-r border-r-gray-300  border-b border-b-[#019943] px-2'>{innerItem.dataOne}</td>
-                                                        <td className='py-1 pl-5 border-r border-r-gray-300  border-b border-b-[#019943] px-2'>{innerItem.dataTwo}</td>
-                                                        <td className='py-1 pl-5 border-r border-r-gray-300  border-b border-b-[#019943] px-2'>{innerItem.dataThree}</td>
-                                                    </tr>
-                                                ))}
-                                            </tbody>
-                                        </table>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                        <tbody>
-                            {item.dataTwo.map((newItem, idx) => (
-                                <tr key={idx}>
-                                    <th className='w-[140px] text-lg bg-[#a8d8a0] text-[#595959] font-semibold text-left pl-3'></th>
-                                    <td className='w-[160px] bg-[#E5F9DB] px-2 py-1 text-base font-semibold border-b border-b-[#019943] text-[#676767]'>{newItem.heading}</td>
-                                    <td>
-                                        <table className='w-full'>
-                                            <tbody>
-                                                {newItem.innerData.map((innerItem, innerIdx) => (
-                                                    <tr key={innerIdx}>
-                                                        <td className='py-1 pl-5 border-r border-r-gray-300  border-b border-b-[#019943] px-2'>{innerItem.dataOne}</td>
-                                                        <td className='py-1 pl-5 border-r border-r-gray-300  border-b border-b-[#019943] px-2'>{innerItem.dataTwo}</td>
-                                                        <td className='py-1 pl-5 border-r border-r-gray-300  border-b border-b-[#019943] px-2'>{innerItem.dataThree}</td>
-                                                    </tr>
-                                                ))}
-                                            </tbody>
-                                        </table>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                        <tbody>
-                            {item.dataThree.map((newItem, idx) => (
-                                <tr key={idx}>
-                                    <th className='w-[140px] text-lg bg-[#a8d8a0] text-[#595959] font-semibold text-left pl-3'></th>
-                                    <td className='w-[160px] bg-[#E5F9DB] px-2 py-1 text-base font-semibold border-b border-b-[#019943] text-[#676767]'>{newItem.heading}</td>
-                                    <td>
-                                        <table className='w-full'>
-                                            <tbody>
-                                                {newItem.innerData.map((innerItem, innerIdx) => (
-                                                    <tr key={innerIdx}>
-                                                        <td className='py-1 pl-5 border-r border-r-gray-300  border-b border-b-[#019943] px-2'>{innerItem.dataOne}</td>
-                                                        <td className='py-1 pl-5 border-r border-r-gray-300  border-b border-b-[#019943] px-2'>{innerItem.dataTwo}</td>
-                                                        <td className='py-1 pl-5 border-r border-r-gray-300  border-b border-b-[#019943] px-2'>{innerItem.dataThree}</td>
-                                                    </tr>
-                                                ))}
-                                            </tbody>
-                                        </table>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                    <div className=' border-b border-b-[#019943] lg:flex w-full' key={index}>
+                        <h2 className=' text-2xl sm:border-l-transparent border-l-[4px] border-l-orange-500 min-w-[150px] md:text-lg bg-[#a8d8a0] text-[#595959] font-semibold text-left pl-3 border-[#019943]'>{item.heading}</h2>
+                        <div className='w-full'>
+                            <div className='flex  w-full'>
+                                {item.dataOne.map((newItem, idx) => (
+                                    <div key={idx} className='flex md:flex-nowrap flex-wrap w-full'>
+                                        <span className='md:max-w-[160px] md:text-left text-center bg-[#E5F9DB] px-2 py-1 text-base font-semibold border-b border-b-[#019943] text-[#676767] min-w-[160px] w-full'>{newItem.heading}</span>
+                                        <div className='w-full'>
+                                            {newItem.innerData.map((innerItem, innerIdx) => (
+                                                <div key={innerIdx} className='flex w-full'>
+                                                    <span className='py-1 pl-5 border-r border-r-gray-300  border-b border-b-[#019943] px-2 w-full '>{innerItem.dataOne}</span>
+                                                    <span className='py-1 pl-5 border-r border-r-gray-300  border-b border-b-[#019943] px-2 w-full'>{innerItem.dataTwo}</span>
+                                                    <span className='py-1 pl-5 border-r border-r-gray-300  border-b border-b-[#019943] px-2 w-full lg:block hidden'>{innerItem.dataThree}</span>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                            <div>
+                                {item.dataTwo.map((newItem, idx) => (
+                                    <div key={idx} className='flex md:flex-nowrap flex-wrap w-full'>
+                                        <span className='md:max-w-[160px] md:text-left text-center bg-[#E5F9DB] px-2 py-1 text-base font-semibold border-b border-b-[#019943] text-[#676767] min-w-[160px] w-full'>{newItem.heading}</span>
+                                        <div className='w-full'>
+                                            {newItem.innerData.map((innerItem, innerIdx) => (
+                                                <div key={innerIdx} className='flex w-full'>
+                                                    <span className='py-1 pl-5 border-r border-r-gray-300  border-b border-b-[#019943] px-2 w-full '>{innerItem.dataOne}</span>
+                                                    <span className='py-1 pl-5 border-r border-r-gray-300  border-b border-b-[#019943] px-2 w-full'>{innerItem.dataTwo}</span>
+                                                    <span className='py-1 pl-5 border-r border-r-gray-300  border-b border-b-[#019943] px-2 w-full lg:block hidden'>{innerItem.dataThree}</span>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                            <div>
+                                {item.dataThree.map((newItem, idx) => (
+                                    <div key={idx} className='flex md:flex-nowrap flex-wrap w-full'>
+                                        <span className='md:max-w-[160px] md:text-left text-center bg-[#E5F9DB] px-2 py-1 text-base font-semibold border-b border-b-[#019943] text-[#676767] min-w-[160px] w-full'>{newItem.heading}</span>
+                                        <div className='w-full'>
+                                            {newItem.innerData.map((innerItem, innerIdx) => (
+                                                <div key={innerIdx} className='flex w-full'>
+                                                    <span className='py-1 pl-5 border-r border-r-gray-300  border-b border-b-[#019943] px-2 w-full '>{innerItem.dataOne}</span>
+                                                    <span className='py-1 pl-5 border-r border-r-gray-300  border-b border-b-[#019943] px-2 w-full'>{innerItem.dataTwo}</span>
+                                                    <span className='py-1 pl-5 border-r border-r-gray-300  border-b border-b-[#019943] px-2 w-full lg:block hidden'>{innerItem.dataThree}</span>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
                 ))}
             </div>
 
