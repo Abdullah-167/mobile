350acc1983f7deafe762c083ca5cd18d2d510a46
Abdullah-167 <abdullahpadhana503@gmail.com>
2023-09-23 16:23:24 +0500

    commit done

--- src/Components/Layout/Navbar.tsx
+++ src/Components/Layout/Navbar.tsx
@@ -35,21 +35,21 @@ const Navbar = ({ toggleSidebar }: any) => {
         <nav className='bg-primary py-4'>
             <Container>
                 {handleForm && (
-                    <div className={`fixed bg-opacity-50 inset-0 pt-5 max-h-screen z-[1000] bg-white`}>
+                    <div className={`fixed bg-opacity-50 inset-0 pt-5 max-h-screen z-[1500] bg-white`}>
                         <Login onClose={handleCloseLogin} />
                     </div>
                 )}
                 <div className='flex justify-between relative gap-5 items-center'>
                     <div className='flex gap-4'>
-                        <span className='text-4xl text-white cursor-pointer' onClick={toggleSidebar}>
+                        <span className='text-2xl sm:text-4xl text-white cursor-pointer' onClick={toggleSidebar}>
                             <TfiMenu />
                         </span>
-                        <Image className='cursor-pointer' src={'/profile.svg'} onClick={handleLogIn} alt={''} width={30} height={30} />
+                        <Image className='cursor-pointer sm:w-full w-6' src={'/profile.svg'} onClick={handleLogIn} alt={''} width={30} height={30} />
                     </div>
                     <Link href={'/'}>
-                        <Image className='cursor-pointer' src={'/newlogo.svg'} alt={''} width={200} height={200} />
+                        <Image className='cursor-pointer max-w-[160px] sm:max-w-full' src={'/newlogo.svg'} alt={''} width={200} height={200} />
                     </Link>
-                    <div className='sm:flex hidden gap-4'>
+                    <div className='lg:flex hidden gap-4'>
                         <div className='flex items-center space-x-4'>
                             <div className=' absolute right-10'>
                                 <div
@@ -61,7 +61,7 @@ const Navbar = ({ toggleSidebar }: any) => {
                                         type='text'
                                         name=''
                                         id=''
-                                        placeholder='Search...'
+                                        placeholder={`${input ? 'Search...' : ''}`}
                                     />
                                     <Image
                                         className='absolute right-3 top-1 cursor-pointer'
@@ -74,7 +74,7 @@ const Navbar = ({ toggleSidebar }: any) => {
                                 </div>
                             </div>
                             <Image
-                                className='cursor-pointer'
+                                className='cursor-pointer lg:block hidden'
                                 src={'/cart.svg'}
                                 alt={''}
                                 width={30}
@@ -82,6 +82,31 @@ const Navbar = ({ toggleSidebar }: any) => {
                             />
                         </div>
                     </div>
+                    <div className='sm:hidden block'>
+                        <div
+                            className={`relative flex items-center transition-all bg-white duration-500 overflow-hidden rounded-full w-7 h-7 `}
+                        >
+                            <Image
+                                className=' flex justify-center mx-auto cursor-pointer'
+                                src={'/magnify.svg'}
+                                alt={''}
+                                width={15}
+                                height={15}
+                                onClick={handleInput}
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div className={`sm:hidden block transition-all duration-500 absolute top-[60px] left-0 z-[1000] w-full text-white  ${input ? ' opacity-100' : ' opacity-0'}`}>
+                    {input && (
+                        <input
+                            className={`outline-none text-white px-4 w-full h-full py-2 bg-[#01c064] placeholder:text-white`}
+                            type='text'
+                            name=''
+                            id=''
+                            placeholder='Search...'
+                        />
+                    )}
                 </div>
             </Container>
         </nav>
