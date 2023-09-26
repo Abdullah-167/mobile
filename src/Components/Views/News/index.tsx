30569aaf2deb67b0f9ea81aef8fab5fc7d5a649b
Abdullah-167 <abdullahpadhana503@gmail.com>
2023-09-26 15:44:22 +0500

    commit done

--- src/Components/Views/News/index.tsx
+++ src/Components/Views/News/index.tsx
@@ -39,7 +39,7 @@ const NewsPage = () => {
                     </div>
                 </div>
             </div>
-            <div className={`z-[1000] transition-all lg:hidden block duration-500 max-h-screen overflow-y-scroll ${isSidebarVisible ? 'lg:relative fixed h-full top-0 translate-x-0' : ' -translate-x-[400px]'}`}>
+            <div className={`z-[50000] transition-all lg:hidden block duration-500 max-h-screen overflow-y-scroll ${isSidebarVisible ? 'lg:relative fixed h-full top-0 translate-x-0' : ' -translate-x-[400px]'}`}>
                 {isSidebarVisible && <SideBar />}
                 <div className='text-xl lg:hidden block rounded-full text-white border border-white p-2 max-w-[39px] z-[1100] top-3 absolute left-[170px] cursor-pointer'
                     onClick={toggleSidebar}
